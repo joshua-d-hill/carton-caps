fd5d8e100d3bb143b3ff35e1007cce02c295358a
Josh <joshua.d.hill@gmail.com>
2022-04-12 19:58:43 -0500

    Data, test, and spec updates in progress

--- nodejs-server/test/test.js
+++ nodejs-server/test/test.js
@@ -2,15 +2,21 @@ const express = require('express');
 const chai = require('chai');
 const request = require('supertest');
 
-describe('POST Create new referral', () => {
+var app = express();
+
+describe('POST /referrals', () => {
     it('should generate a new referral with appropriate data', () => {
         request(app)
-        .post('')
-        .send({})
-        .expect(201)
-        .then((res) => {
-         expect(res.headers.location).to.be.eql('referral');
-         // additional validations as required
+        .post('/referrals')
+        .send({"id": "7","referer": "70","referee": "71","code": "TRXCLN","status": "Sent","link": "https://cartoncaps.link/abfilefa90p?referral_code=TRXCLN","method": "Email"})
+         expect(201, done);
     });
- });
+});
+
+describe('GET /referrals', function() {
+    it('should fetch all referrals', function(done) {
+        request(app)
+        .get('/referrals')
+        .expect(200, done);
+    });
 });
